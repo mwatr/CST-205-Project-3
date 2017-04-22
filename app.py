bff4208ee8889308aafd468ce133df3f8d9ed99e
Conrado Aguilar <coaguilar@csumb.edu>
2017-04-22 23:06:35 +0000

    Naming

--- app.py
+++ app.py
@@ -96,12 +96,13 @@ if not app.debug:
 #----------------------------------------------------------------------------#
 
 # Default port:
+'''
 if __name__ == '__main__':
     app.run()
-
+'''
 # Or specify port manually:
-'''
+
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
-'''
+
